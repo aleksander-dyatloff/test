9c41186e5afe79c519586ad9646aee129b0bf77e
Aleksander Dyatloff <alex.d@halo-lab.team>
2025-04-14 18:57:13 +0300

    update prismic config

--- prismicio.ts
+++ prismicio.ts
@@ -7,6 +7,9 @@ export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint);
 
 export const createClient = () => {
   const client = prismic.createClient(sm.apiEndpoint, {
+    fetchOptions: {
+      cache: "no-store",
+    },
     accessToken:
       "MC5aX0VyT0JFQUFDSUE3NG56.77-977-9QO-_vT3vv73vv73vv70i77-977-977-9Ug7vv71_77-9DxJY77-977-977-977-9MQHvv71mSFcJ77-9",
   });
